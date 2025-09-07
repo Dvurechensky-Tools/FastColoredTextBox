f31ca3adb61851a5586f340b0749f8852547b149
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-09-07 10:49:53 +0300

    Auto-update headers via pre_commit.ps1 - 2025-09-07

--- FastColoredTextBox/EncodingDetector.cs
+++ FastColoredTextBox/EncodingDetector.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 06 сентября 2025 11:47:19
- * Version: 1.0.191
+ * Last Updated: 07 сентября 2025 10:49:37
+ * Version: 1.0.192
  */
 
 //          Copyright Tao Klerks, 2010-2012, tao@klerks.biz         
