c76386922f46a2b2e89c5f9b2227c5afc36ceb3e
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-09-29 06:53:27 +0300

    Auto-update headers via pre_commit.ps1 - 2025-09-29

--- FastColoredTextBoxNet8/ExportToHTML.cs
+++ FastColoredTextBoxNet8/ExportToHTML.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 28 сентября 2025 21:15:30
- * Version: 1.0.213
+ * Last Updated: 29 сентября 2025 06:53:11
+ * Version: 1.0.214
  */
 
 using System.Text;
