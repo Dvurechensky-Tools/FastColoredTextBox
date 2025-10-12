aa775b7bd7a3e73692dfccda07b107d4a66a9131
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-10-12 11:00:16 +0300

    Auto-update headers via pre_commit.ps1 - 2025-10-12

--- FastColoredTextBox/FileTextSource.cs
+++ FastColoredTextBox/FileTextSource.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 11 октября 2025 08:50:36
- * Version: 1.0.226
+ * Last Updated: 12 октября 2025 11:00:02
+ * Version: 1.0.227
  */
 
 //#define debug
