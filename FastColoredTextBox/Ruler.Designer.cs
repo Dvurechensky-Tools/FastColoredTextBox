f1eeebd79096bd5764f9b1462388407597037631
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-11-03 06:53:12 +0300

    Auto-update headers via pre_commit.ps1 - 2025-11-03

--- FastColoredTextBox/Ruler.Designer.cs
+++ FastColoredTextBox/Ruler.Designer.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 02 ноября 2025 17:24:18
- * Version: 1.0.248
+ * Last Updated: 03 ноября 2025 06:52:57
+ * Version: 1.0.249
  */
 
 namespace FastColoredTextBoxNS
