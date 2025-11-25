3c197cd861db43e4396474a7f93249c09c9bf4b3
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-11-25 06:52:56 +0300

    Auto-update headers via pre_commit.ps1 - 2025-11-25

--- Tester/CustomTextSourceSample2.cs
+++ Tester/CustomTextSourceSample2.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 24 ноября 2025 12:27:34
- * Version: 1.0.270
+ * Last Updated: 25 ноября 2025 06:52:46
+ * Version: 1.0.271
  */
 
 using System;
