e97d622c6206f39500889325199a2eafbbf52218
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-12-17 15:26:30 +0300

    Auto-update headers via pre_commit.ps1 - 2025-12-17

--- TesterNet8/SimplestSyntaxHighlightingSample.cs
+++ TesterNet8/SimplestSyntaxHighlightingSample.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 16 декабря 2025 06:53:06
- * Version: 1.0.292
+ * Last Updated: 17 декабря 2025 15:26:15
+ * Version: 1.0.293
  */
 
 using System.Drawing;
