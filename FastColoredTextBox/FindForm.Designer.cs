ae31f1f15554ea06058d09ef6b7209a59a9d14a2
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-02-15 06:53:22 +0300

    Auto-update headers via pre_commit.ps1 - 2026-02-15

--- FastColoredTextBox/FindForm.Designer.cs
+++ FastColoredTextBox/FindForm.Designer.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 14 февраля 2026 10:04:07
- * Version: 1.0.352
+ * Last Updated: 15 февраля 2026 06:53:06
+ * Version: 1.0.353
  */
 
 namespace FastColoredTextBoxNS
