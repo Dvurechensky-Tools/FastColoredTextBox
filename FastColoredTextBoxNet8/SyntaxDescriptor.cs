45307fb20c07d18aeb5bcecdbea9c5700bfa03f0
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-02-28 15:56:56 +0300

    Auto-update headers via pre_commit.ps1 - 2026-02-28

--- FastColoredTextBoxNet8/SyntaxDescriptor.cs
+++ FastColoredTextBoxNet8/SyntaxDescriptor.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 27 февраля 2026 09:43:53
- * Version: 1.0.365
+ * Last Updated: 28 февраля 2026 15:56:41
+ * Version: 1.0.366
  */
 
 using System.Collections.Generic;
